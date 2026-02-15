67d2c46d47bee8603cd8336651194deba4ce2fe5
Selim Bucher <selim.bucher@slimmer.ch>
2026-02-15 16:15:47 +0100

    refactor(tools): replace "return 1" with std::abort() in HIP_CHECK
    This is in general more robust because it does not assume the scope of the macro.

--- experiments/check_gpu.cpp
+++ experiments/check_gpu.cpp
@@ -7,7 +7,7 @@ do { \
     hipError_t err = call; \
     if (err != hipSuccess) { \
         std::cerr << "HIP Error: " << hipGetErrorString(err) << " at line " << __LINE__ << std::endl; \
-        return 1; \
+        std::abort(); \
     } \
 } while(0)
 
